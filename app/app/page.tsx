8c37573cd3c81409d1ffd1d18fe8b3986bea9d43
phil100742 <phileas.monachon@gmail.com>
2025-08-20 16:58:05 +0200

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Bienvenue chez Didi 🍰</h1>
-      <p>La boutique de pâtisseries arrive bientôt !</p>
-    </main>
-  );
-}
